1b0b6de82d7c2b117ea8b84eda1ce23cbf418a79
JorgeHohhe <jorge.costa.profwork@gmail.com>
2022-08-19 17:32:07 -0300

    Add Stock Price Graph

--- src.py
+++ src.py
@@ -20,16 +20,28 @@ def get_daily_return_value(stock_price, volatility):
     return new_stock_price, daily_return * 100
 
 def plot_graph_from_dataframe(df, days_of_data):
-    x = list(range(days_of_data))
+    fig, axs = plt.subplots(2)
+    fig.suptitle('Stocks Data')
+
+    x_daily_return = list(range(days_of_data))
+    x_stock_price = x_daily_return[:]
+    x_stock_price.append(len(x_daily_return))
     for column in df.columns:
-        y = df[column]
-        plt.plot(x, y)
+        y_daily_return = df[column]
+        axs[0].plot(x_daily_return, y_daily_return)
+        axs[0].grid()
+        axs[0].set_ylabel('Daily Return (%)')
+
+        y_stock_price = [1000]
+        for i, daily_return in enumerate(y_daily_return):
+            y_stock_price.append(y_stock_price[i] * (1+daily_return/100))
+        axs[1].plot(x_stock_price, y_stock_price)
+        axs[1].grid()
+        axs[1].set_xlabel('Days')
+        axs[1].set_ylabel('Stock Price ($)')
+        axs[1].legend(df.columns)
     
-    plt.grid()
-    plt.xlabel('Days')
-    plt.ylabel('Daily Return (%)')
-    plt.legend(df.columns)
-    plt.savefig(f'output/Sample-{len(df.columns)}stocks-{days_of_data}days.png')
+    fig.savefig(f'output/Sample-{len(df.columns)}stocks-{days_of_data}days.png')
 
 if __name__ == "__main__":
     create_directory('output')
@@ -45,7 +57,7 @@ if __name__ == "__main__":
         df[stock_name] = pd.Series([], dtype=np.float64)
         for i in range(days_of_data): 
             stock_price, daily_return = get_daily_return_value(stock_price, volatility[k]) # 2%
-            df.loc[i, stock_name] = daily_return 
+            df.loc[i, stock_name] = daily_return
     df = df.drop(['Placeholder'], axis=1)
     print(df)
 
